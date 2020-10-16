ea5c96cb5efda4fb54a346696e2f6f3dc632b3e3
Farzad Sunavala <farzadsunavala@chevron.com>
2020-10-16 10:30:32 -0500

    auth token

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import Login from "./Login";
+import { getTokenFromUrl } from "./spotify";
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+    }
+
+    console.log(" I HAVE A TOKEN >>>>", token);
+  }, []);
+  
   return (
-    <div className="app">
-      <h1>spotify clone</h1>
-    </div>
+    <div className="app">{token ? <h1>i am logged in</h1> : <Login />}</div>
   );
 }
 
